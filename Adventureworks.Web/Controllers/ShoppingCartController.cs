d54f4c0f3d14dde253edd6df17fc28db9dc11919
Dennis Traub <dennis.traub@gmail.com>
2013-03-22 10:39:08 +0100

    Removed Shopping Cart Repository

--- Adventureworks.Web/Controllers/ShoppingCartController.cs
+++ Adventureworks.Web/Controllers/ShoppingCartController.cs
@@ -12,12 +12,10 @@ namespace Adventureworks.Web.Controllers
     public class ShoppingCartController : Controller
     {
         private readonly IProductRepository _productRepository;
-        private readonly IShoppingCartRepository _shoppingCartRepository;
 
         [ImportingConstructor]
-        public ShoppingCartController(IShoppingCartRepository shoppingCartRepository, IProductRepository productRepository)
+        public ShoppingCartController(IProductRepository productRepository)
         {
-            this._shoppingCartRepository = shoppingCartRepository;
             this._productRepository = productRepository;
         }
 
@@ -27,10 +25,22 @@ namespace Adventureworks.Web.Controllers
         public ActionResult Index()
         {
             var cartItems =
-                this._shoppingCartRepository.GetCartItemsByID(this.HttpContext.User.Identity.Name).AsEnumerable();
+                GetCartItemsByID(this.HttpContext.User.Identity.Name).AsEnumerable();
             ViewBag.CartTotal = GetTotal(this.HttpContext.User.Identity.Name);
 
             return View(cartItems);
+        }
+
+        public IQueryable<ShoppingCartItem> GetCartItemsByID(string shoppingCartID)
+        {
+            using (var db = new AdventureWorks2008R2Entities())
+            {
+                var cartItems = from cart in db.ShoppingCartItems
+                                where cart.ShoppingCartID == shoppingCartID
+                                select cart;
+
+                return cartItems;
+            }
         }
 
         private decimal GetTotal(string shoppingCartID)
@@ -49,7 +59,7 @@ namespace Adventureworks.Web.Controllers
 
         public ActionResult AddToCart(int id)
         {
-            this._shoppingCartRepository.AddToCart(this.HttpContext.User.Identity.Name, id, 1);
+            AddToCart(this.HttpContext.User.Identity.Name, id, 1);
 
             return RedirectToAction("Index");
         }
@@ -57,7 +67,7 @@ namespace Adventureworks.Web.Controllers
         [HttpPost]
         public JsonResult JsonAddToCart(int id)
         {
-            this._shoppingCartRepository.AddToCart(this.HttpContext.User.Identity.Name, id, 1);
+            AddToCart(this.HttpContext.User.Identity.Name, id, 1);
 
             Product product = _productRepository.GetProductById(id);
 
@@ -67,6 +77,35 @@ namespace Adventureworks.Web.Controllers
                 product.Name,
                 LargeUrl = VirtualPathUtility.ToAbsolute("~/Image/ProductThumbnail?productPhotoID=" + product.ProductProductPhotoes.FirstOrDefault<ProductProductPhoto>().ProductPhotoID)
             });
+        }
+
+        public void AddToCart(string shoppingCartID, int productID, int quantity)
+        {
+            using (var db = new AdventureWorks2008R2Entities())
+            {
+                ShoppingCartItem myItem =
+                    (from c in db.ShoppingCartItems
+                     where c.ShoppingCartID == shoppingCartID && c.ProductID == productID
+                     select c).FirstOrDefault();
+                if (myItem == null)
+                {
+                    var cartadd = new ShoppingCartItem
+                        {
+                            ShoppingCartID = shoppingCartID,
+                            Quantity = quantity,
+                            ProductID = productID,
+                            DateCreated = DateTime.Now,
+                            ModifiedDate = DateTime.Now
+                        };
+                    db.ShoppingCartItems.AddObject(cartadd);
+                }
+                else
+                {
+                    myItem.Quantity += quantity;
+                }
+
+                db.SaveChanges();
+            }
         }
 
         //
@@ -131,7 +170,7 @@ namespace Adventureworks.Web.Controllers
         public JsonResult GetCartItems()
         {
             var cartItems =
-                this._shoppingCartRepository.GetCartItemsByID(this.HttpContext.User.Identity.Name).AsEnumerable();
+                GetCartItemsByID(this.HttpContext.User.Identity.Name).AsEnumerable();
 
             var dataRows = (cartItems.Select(cartItem => new {
                                                                  product = new {
